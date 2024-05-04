9eb14262483d4fcde7e306bc0f9f667679952d6e
Marc MAURICE <marc-github@pub.positon.org>
2024-05-04 23:44:48 +0200

    script to upload a script to a tasmota module

---
+++ tasmota-upload-script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# here is a simple script to upload a script to tasmota
+
+script="$1"
+ip="$2"
+
+[ "$script" = "" -o "$ip" = "" ] && echo "Syntax $0 scriptFile tasmotaIp" && exit 1
+
+
+grep -v '^;' "$script" >/tmp/$0.tmp
+
+# --trace-ascii -
+curl -v -F c1=on -F t1="</tmp/$0.tmp" -F save= "$ip/ta"

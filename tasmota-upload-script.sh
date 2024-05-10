bf03e3512c6ec1de3e07874823b4cbba8192acb8
Marc MAURICE <marc-github@pub.positon.org>
2024-05-10 23:00:21 +0200

    ignore comments begining with spaces

--- tasmota-upload-script.sh
+++ tasmota-upload-script.sh
@@ -7,7 +7,7 @@ ip="$2"
 [ "$script" = "" -o "$ip" = "" ] && echo "Syntax $0 scriptFile tasmotaIp" && exit 1
 
 
-grep -v '^;' "$script" >/tmp/$0.tmp
+grep -v '^ *;' "$script" >/tmp/$0.tmp
 
 # --trace-ascii -
 curl -v -F c1=on -F t1="</tmp/$0.tmp" -F save= "$ip/ta"

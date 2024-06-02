0cc30a376112576e20f0d79e40e86a905c8d375b
Marc MAURICE <marc-github@pub.positon.org>
2024-06-02 22:15:01 +0200

    upload script: generate in a tmp.scr in current dir

--- tasmota-upload-script.sh
+++ tasmota-upload-script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 # here is a simple script to upload a script to tasmota
 
 slimit=2560
@@ -8,18 +8,22 @@ ip="$2"
 
 [ "$script" = "" -o "$ip" = "" ] && echo "Syntax $0 scriptFile tasmotaIp" && exit 1
 
-# comments to remove: line start, spaces, ';', but no 'k' otherwise we keep the comment
-grep -v '^ *;[^k]' "$script" >/tmp/$0.tmp
+cd "$(dirname $0)"
 
-sedFile=$(dirname $0)/sed.cnf
+# comments to remove: line start, spaces, ';', but no 'k' otherwise we keep the comment
+#grep -v '^ *;[^k]' "$script" >/tmp/$0.tmp
+grep -v -e '^ *;' -e '^ *$' "$script" >tmp.scr
+#sed -i '/^ *;k/s/;k;/;/' /tmp/$0.tmp
+
+sedFile=sed.cnf
 if [ -f "$sedFile" ]; then
 	while IFS= read -r line
 	do
-		sed -i "$line" /tmp/$0.tmp
+		sed -i "$line" tmp.scr
 	done < "$sedFile"
 fi
 
-size=$(stat -c '%s' "/tmp/$0.tmp")
+size=$(stat -c '%s' tmp.scr)
 if [ "$size" -gt "$slimit" ]; then
 	echo "script too big: $size > $slimit" && exit 2
 fi
@@ -27,10 +31,10 @@ fi
 echo=""
 if [ "$ip" = "test" ]; then
 	echo="echo"
-	cat /tmp/$0.tmp
+	cat tmp.scr
 fi
 
 # --trace-ascii -
-$echo curl -v -F c1=on -F t1="</tmp/$0.tmp" -F save= "$ip/ta"
+$echo curl -v -F c1=on -F t1="<tmp.scr" -F save= "$ip/ta"
 
 echo "size: $size <= $slimit"

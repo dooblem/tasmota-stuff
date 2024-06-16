e8786b275dfb3f4db65de16a1bf0381f68965bf5
Marc MAURICE <marc-github@pub.positon.org>
2024-06-16 09:52:27 +0200

    refactoring tasmota push script

--- tasmota-scr.sh
+++ tasmota-scr.sh
@@ -3,16 +3,6 @@
 
 slimit=2560
 
-script="$1"
-ip="$2"
-action="$3"
-
-[ "$script" = "" -o "$ip" = "" -o \( "$action" != "" -a "$action" != get \) ] && echo "Syntax $0 scriptFile tasmotaIp|gen [get]" && exit 1
-
-cd "$(dirname $0)"
-
-cp "$script" tmp.scr
-
 applySedFile() {
   if [ -f "$1" ]; then
     grep -v -e '^#' -e '^ *$' "$1" | while IFS= read -r line
@@ -22,33 +12,93 @@ applySedFile() {
   fi
 }
 
-applySedFile "hosts/$ip.sed"
-applySedFile "$script.sed"
+# output: tmp.scr
+sedScr() {
+  local script="$1"
+  local ip="$2"
 
-size=$(stat -c '%s' tmp.scr)
-if [ "$size" -gt "$slimit" ]; then
-	echo "script too big: $size > $slimit" && exit 2
+  cp "$script" tmp.scr
+  applySedFile "hosts/$ip.sed"
+  applySedFile "$script.sed"
+
+  size=$(stat -c '%s' tmp.scr)
+  if [ "$size" -gt "$slimit" ]; then
+    echo "script too big: $size > $slimit" && exit 2
+  fi
+
+  echo "Generated ./tmp.scr size: $size <= $slimit"
+}
+
+# output: remote.scr
+getScr() {
+  # get the remote script
+  curlArgs=$(getCurlArgs "$1")
+  echo "Downloading ./remote.scr"
+  curl -sSf $curlArgs "$1/s10" | sed -e 's/^.*<textarea[^>]*>//' -e 's|</textarea><script.*$||' >remote.scr
+}
+
+pushScr() {
+  local ip="$1"
+  local script="$2"
+
+  sedScr "$script" "$ip"
+
+  getScr "$ip"
+
+  if diff -u remote.scr tmp.scr; then
+    echo "remote and generated scripts are identical"
+  fi
+
+  read -p "Upload script ??? Enter to continue. Ctrl+C to abort."
+
+  curlArgs=$(getCurlArgs "$ip")
+
+  # --trace-ascii -
+  curl -v -F c1=on -F t1="<tmp.scr" -F save= $curlArgs "$ip/ta"
+}
+
+getCurlArgs() {
+  if [ -f "hosts/$1.curlargs" ]; then
+    cat "hosts/$1.curlargs"
+  fi
+}
+
+syntax() {
+  echo "Syntax:
+
+# push a script to a device
+$0 push tasmotaIp scriptFile
+
+# get the script from a device
+$0 get tasmotaIp
+
+# apply sed to file
+$0 sed scriptFile
+$0 sed tasmotaIp scriptFile
+" && exit 1
+}
+
+cd "$(dirname $0)"
+
+action="$1"
+
+if [ "$action" = get ]; then
+  [ "$2" = "" ] && syntax
+  getScr "$2"
+
+elif [ "$action" = sed ]; then
+  [ "$2" = "" ] && syntax
+  if [ "$3" = "" ]; then
+    sedScr "$2"
+  else
+    sedScr "$3" "$2"
+  fi
+
+elif [ "$action" = push ]; then
+  [ "$2" = "" -o "$3" = "" ] && syntax
+  echo pushScr "$2" "$3"
+  pushScr "$2" "$3"
+
+else
+  syntax
 fi
-
-echo "size: $size <= $slimit"
-
-[ "$ip" = "gen" ] && exit
-
-curlArgs=
-if [ -f "hosts/$ip.curlargs" ]; then
-	curlArgs=$(cat "hosts/$ip.curlargs")
-fi
-
-# get the remote script
-curl -sSf $curlArgs "$ip/s10" | sed -e 's/^.*<textarea[^>]*>//' -e 's|</textarea><script.*$||' >remote.scr
-
-if diff -u remote.scr tmp.scr; then
-  echo "remote and generated scripts are identical"
-fi
-
-[ "$action" = get ] && exit
-
-read -p "Upload script ??? Enter to continue. Ctrl+C to abort."
-
-# --trace-ascii -
-curl -v -F c1=on -F t1="<tmp.scr" -F save= $curlArgs "$ip/ta"
